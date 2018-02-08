5df07128b7d3e8d44ad0929848d4f33461d93299
pedro12374 <pedrohaerter@hotmail.com>
2018-02-08 13:24:34 -0200

    basic att

--- Scanner.py
+++ Scanner.py
@@ -2,42 +2,35 @@ import cv2,os,glob,time
 start_time = time.time()
 from PIL import Image
 import numpy as np
-'''
-path = os.getcwd()
-os.chdir(path+'/Auvers sur Oise (1890)')
-path = os.getcwd()
-k = glob.glob('*.jpg')
-print k
-'''
-#Paint = open('Pinturas.tsv','w')
-#for pintura in range(len(k)):
-
-def ler_Pasta(caminho):
+totR=[]
+totB=[]
+totG=[]
+def ler_Pasta(caminho): #Mostra todos os aquivos com a extensao .jpg na pasta 'caminho'
 	os.chdir(caminho)
 	temp = glob.glob('*.jpg')
 	return temp
 
-def Contar_Pixels(img):
-
+def Contar_Pixels(ipt): #Conta quantos pixels de cada cor tem
+	img = cv2.imread(ipt)
 	altura = len(img)
 	largura = len(img[0])
 	for i in range(altura):
 		for j in range(largura):
 			px = img[i,j]
-			totR.append(px[0])
-			totB.append(px[2])
+			totB.append(px[0])
 			totG.append(px[1])
+			totR.append(px[2])
 
-def Calcular_Media(vetor):
+def Calcular_Media(vetor): #Faz uma media do vetor colocado
 	temp = sum(vetor)/len(vetor)
 	return temp
 
-def Gerar_Media(Nome,valores):
+def Gerar_Media(Nome,r,g,b): #Gera um arquivo png com intitulado com o "Nome", usando os valores de rgb colocados
 	im = Image.new("RGB", (128, 128))
 	pix = im.load()
 	for x in range(128):
 	    for y in range(128):
-	        pix[x,y] = (valores[0],valores[1],valores[2])
+	        pix[x,y] = (r,g,b)
 	im.save(Nome+".png", "PNG")
 
 def Distribuicao(V_Entrada,V_Saida,tamanho):
@@ -52,18 +45,9 @@ def Gerar_Distribuicao(Nome,vetor_Saida,tamanho):
 		temp.write( str(i)+ '\t'+str(vetor_Saida[i])+'\n')
 
 
-imagem = cv2.imread('Romero.jpg')
 
-totR = totG = totB = GsR = GsG = GsB = []
-
-
-print("--- %s seconds ---" % (time.time() - start_time))
-
-
-
-
-
-#cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-#cv2.imshow('image',img)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+k=[]
+u = Contar_Pixels('Romero.jpg')
+print totG
+Distribuicao(totG,k,256)
+print k
